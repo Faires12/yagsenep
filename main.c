5de616310374d54165cf42441b8e3c9899794467
Fares Solibie <55356219+Faires12@users.noreply.github.com>
2021-08-25 21:28:49 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -19,13 +19,18 @@ int main()
 
         switch(opc){
             case 1:
+                printf("Em construcao...\n");
                 break;
             case 2:
                 printf("History\n");
                 break;
             case 3:
+                for(int i = 1; i <= 5; i++){
+                    printf("%d lugar\n", i);
+                }
                 break;
             case 4:
+                printf("Fares Solibie\nGabriel Felipe\nHenrique Amaral");
                 break;
             case 5:
                 run = false;
